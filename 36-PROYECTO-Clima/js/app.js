a3fabb5cc6b18933c3151873e49d67289ea82418
Jose Luis <jluisvazquez.jv@gmail.com>
2020-09-15 13:33:20 -0500

    224 Validando el formulario

--- 36-PROYECTO-Clima/js/app.js
+++ 36-PROYECTO-Clima/js/app.js
@@ -11,5 +11,42 @@ window.addEventListener('load', () => {
 function buscarClima(e) {
     e.preventDefault();
 
-    console.log('Buscando el Clima...');
+    // validar
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
+
+    if(ciudad === '' || pais === '') {
+        //hubo un error
+        mostrarError('Ambos campos son obligatorios');
+
+        return;
+    }
+
+
+
+    // Consultariamos la API
+}
+
+function mostrarError(mensaje) {
+    const alerta = document.querySelector('.bg-red-100');
+
+    if(!alerta) {
+        //crear una alerta
+        const alerta = document.createElement('div');
+
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded',
+        'max-w-md', 'mx-auto', 'mt-6', 'text-center' );
+    
+        alerta.innerHTML = `
+            <strong class="font-bold">Error!</strong>
+            <span class="block">${mensaje}</span>
+        `;
+
+        container.appendChild(alerta);
+
+        // se elimina la alerta despues de 5 seg
+        setTimeout(() => {
+            alerta.remove();
+        }, 5000);
+    }
 }
